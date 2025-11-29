e40491a03e0df093e027b308cf3dde5d8de34137
Nareshmalviya09 <Nareshmalviya4321@gmail.com>
2025-11-29 23:36:53 +0530

    new file added

--- state-variable-hooks/src/App.jsx
+++ state-variable-hooks/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+ import React from 'react'
+ 
+//  step-1
+import {useState} from 'react'
+ 
+ const App = () => {
+  // step-2
+  const [a, seta]=useState(0)
+  const increment=()=>{
+    seta(a+1)
+  }
+  const decrement=()=>{
+    seta(a-1)
+  }
+   return (
+     <div className='flex flex-col justify-center items-center h-screen'>
+             <h1>my count is {a}</h1>
+      <button className='bg-purple-600 rounded-sm px-4 my-4' onClick={increment}>increment</button>
+      
+      <button className='bg-purple-600 rounded-sm px-4' onClick={decrement}>decrement</button>
+     </div>
+   )
+ }
+ 
+ export default App
